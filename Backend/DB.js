bc2b8a927cc8eb752b64573c58873c455c633b78
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-05-25 13:17:31 +0530

    design db schema

---
+++ Backend/DB.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectID = mongoose.ObjectID;
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  userName: String,
+  email: { type: String, unique: true },
+  password: String,
+  userID: ObjectID,
+  userCourses: [courseSchema],
+});
+
+const courseSchema = new Schema({
+  title: String, // course name
+  description: String, // About Course
+  courseContent: String, // Course Videos
+  courseLearning: String, // What will you learn
+  courseAuthorDetail: String, // Author Name
+  coursePrice: Number, // course price
+  courseValidatiy: String, // course active duration
+  courseID: ObjectID,
+  courseMaterial: String, // What all things will be aviable in this course
+  courseRequirements: String, // What all are the prerequsities
+});
+
+const adminSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  userName: String,
+  password: String,
+  adminID: ObjectID,
+  adminCourses: [courseSchema],
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("courses", courseSchema);
+
+module.exports = {
+  userModel,
+  adminModel,
+  courseModel,
+};
